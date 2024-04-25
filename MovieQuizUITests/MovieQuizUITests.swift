9b1c657fb085fe27190a9c00420681fed2138288
Dioss87 <m.dioniss@gmail.com>
2024-04-25 13:45:55 +0300

    .

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -4,82 +4,59 @@ final class MovieQuizUITests: XCTestCase {
     
     var app: XCUIApplication!
 
-        override func setUpWithError() throws {
-            try super.setUpWithError()
-            
-            app = XCUIApplication()
-            app.launch()
-            continueAfterFailure = false
-        }
-
-        override func tearDownWithError() throws {
-            try super.tearDownWithError()
-            app.terminate()
-            app = nil
-        }
+    override func setUpWithError() throws {
+        try super.setUpWithError()
         
-        func testYesButton() {
-            sleep(3)
-            let firstPoster = app.images["Poster"]
-            let firstPosterData = firstPoster.screenshot().pngRepresentation
-             
-            app.buttons["Yes"].tap()
-            sleep(3)
-            
-            let secondPoster = app.images["Poster"]
-            let secondPoosterData = secondPoster.screenshot().pngRepresentation
-            
-            XCTAssertNotEqual(firstPosterData, secondPoosterData)
-        }
-        
-        func testNoButton() {
-            sleep(3)
-            let firstPoster = app.images["Poster"]
-            let firstPosterData = firstPoster.screenshot().pngRepresentation
-             
-            app.buttons["No"].tap()
-            sleep(3)
-            
-            let secondPoster = app.images["Poster"]
-            let secondPoosterData = secondPoster.screenshot().pngRepresentation
-            
-            XCTAssertNotEqual(firstPosterData, secondPoosterData)
-        }
-        
-        func testIndexLabel() {
-            sleep(2)
-            let indexLabel = app.staticTexts["Index"]
-            XCTAssertEqual(indexLabel.label, "1/10")
-        }
-        
-    func testGameAlert() {
-        sleep(2)
-        for _ in 1..<10 {
-            app.buttons["Yes"].tap()
-            sleep(2)
-<<<<<<< HEAD
-        }
-        let gameAlert = app.alerts["GameOverAlert"]
-        
-        XCTAssertEqual(gameAlert.label, "Этот раунд окончен!", "Некорректный заголовок")
-        XCTAssertEqual(gameAlert.buttons.firstMatch.label, "Сыграть еще раз", "Некорректный текст кнопки")
+        app = XCUIApplication()
+        app.launch()
+        continueAfterFailure = false
     }
-            
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        app.terminate()
+        app = nil
+    }
+    
+    func testYesButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+         
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPoosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPoosterData)
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+         
+        app.buttons["No"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPoosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPoosterData)
+    }
+    
+    func testIndexLabel() {
+        sleep(2)
+        let indexLabel = app.staticTexts["Index"]
+        XCTAssertEqual(indexLabel.label, "1/10")
+    }
+
     func testGameAlertDismiss() {
         sleep(2)
         for _ in 1...10 {
             app.buttons["No"].tap()
             sleep(2)
-=======
-            for _ in 1..<10 {
-                app.buttons["Yes"].tap()
-                sleep(2)
-            }
-            let gameAlert = app.alerts["GameOverAlert"]
-            
-            XCTAssertEqual(gameAlert.label, "Этот раунд окончен!", "Некорректный заголовок")
-            XCTAssertEqual(gameAlert.buttons.firstMatch.label, "Сыграть еще раз")
->>>>>>> 9c2bb22c090e59f7d43e20f8e5a40586af54815d
         }
         
         let gameAlert = app.alerts["GameOverAlert"]
@@ -90,28 +67,14 @@ final class MovieQuizUITests: XCTestCase {
         
         XCTAssertFalse(gameAlert.exists)
         XCTAssertTrue(indexLabel.label == "1/10")
-            }
-<<<<<<< HEAD
-
-=======
-            
-            let gameAlert = app.alerts["GameOverAlert"]
-            gameAlert.buttons["Сыграть еще раз"]
-            sleep(2)
-            
-            let indexLabel = app.staticTexts["Index"]
-            
-            XCTAssertFalse(gameAlert.exists)
-            XCTAssertTrue(indexLabel.label == "1/10")
-        }
->>>>>>> 9c2bb22c090e59f7d43e20f8e5a40586af54815d
-        
-        func testQuestionLabel() {
-            let questionLabel = app.staticTexts["Question"]
-            XCTAssertTrue(questionLabel.waitForExistence(timeout: 5), "Лейбл не появился")
-            
-            let questionText = questionLabel.label
-            
-            XCTAssertFalse(questionText.isEmpty, "Текст в лейбле отсутствует")
-        }
     }
+    
+    func testQuestionLabel() {
+        let questionLabel = app.staticTexts["Question"]
+        XCTAssertTrue(questionLabel.waitForExistence(timeout: 5), "Лейбл не появился")
+        
+        let questionText = questionLabel.label
+        
+        XCTAssertFalse(questionText.isEmpty, "Текст в лейбле отсутствует")
+    }
+}
